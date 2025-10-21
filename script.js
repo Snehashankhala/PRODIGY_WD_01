fc4de67b2fd35aaf5e617d1ae5cb9be6167d72ef
Sneha Shankhala <snehashankhala@gmail.com>
2025-10-21 17:46:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll",function(){
     const navbar = this.document.quarySelection(".navbar");
     if(this.window.scrollY>50){
-        navbar.classList.add("scrolled");
+        navbar.classList.add("scrolled"); 
     }else{
         navbar.classList.remove("scrolled");
     }
